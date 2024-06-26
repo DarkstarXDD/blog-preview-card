6d628ed232868c554e5c870d4d40b411162040ef
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-06-26 23:17:29 +0530

    add html and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        figtree: ["Figtree", "sans-serif"],
+      },
+
+      colors: {
+        "primary-400": "hsl(47, 89%, 63%)",
+
+        "neutral-100": "hsl(0, 0%, 100%)",
+        "neutral-200": "hsl(0, 0%, 34%)",
+        "neutral-900": "hsl(0, 0%, 7%)",
+      },
+
+      boxShadow: {
+        "3xl": "8px 8px",
+        "4xl": "12px 12px",
+      },
+    },
   },
   plugins: [],
 }
